4755629881442e5330a35d19e42ff1aa0b253a1a
srilekha-cmyk <bsrilekha75@gmail.com>
2026-07-06 10:37:20 +0530

    Implement character index printing in program.py
    Add a loop to print index and character of a string

---
+++ program.py
@@ -0,0 +1,11 @@
+s="Python"
+for i in range(len(s)):
+    print("Index:",i,"Character:",s[i])
+
+#Output:
+#Index: 0 Character: P
+#Index: 1 Character: y
+#Index: 2 Character: t
+#Index: 3 Character: h
+#Index: 4 Character: o
+#Index: 5 Character: n
