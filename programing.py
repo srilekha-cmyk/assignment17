3ba8bd4b548a8f1e9824d4b66c06a1755a9dfea8
srilekha-cmyk <bsrilekha75@gmail.com>
2026-07-06 10:33:17 +0530

    Create  programing.py

---
+++ programing.py
@@ -0,0 +1,10 @@
+numbers=[1,2,3,4,5]
+for i in numbers:
+    print("Number:",i,"Square:",i*i)
+
+#Output:
+#Number: 1 Square: 1
+#Number: 2 Square: 4
+#Number: 3 Square: 9
+#Number: 4 Square: 16
+#Number: 5 Square: 25
